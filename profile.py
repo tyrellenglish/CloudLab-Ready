aa19a302c60509f262bd967c31c72853020d077f
tyrellenglish <112589559+tyrellenglish@users.noreply.github.com>
2026-02-08 12:14:56 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,1 +1,20 @@
+import geni.portal as portal
+import geni.rspec.pg as pg
+
+pc = portal.Context()
+rspec = pg.Request()
+
+
+node = rspec.RawPC("node")
+
+
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
+
+
+node.addService(pg.Execute(
+    shell="bash",
+    command="bash /local/repository/scripts/cloudlab/bootstrap.sh"
+))
+
+pc.printRequestRSpec(rspec)
 
